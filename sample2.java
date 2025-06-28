946deba1dbdb4931052fa5541bc576cc213b91e3
Jahnavi <njanu2128@gmail.com>
2025-06-28 16:31:41 +0530

    Update sample2.java

--- sample2.java
+++ sample2.java
@@ -1,6 +1,8 @@
 public class sample2 {
     public static void main(String[] args) {
         System.out.println("Hello, World from sample2!");
+        System.out.println("i made some changes");
     }
+    
     
 }
